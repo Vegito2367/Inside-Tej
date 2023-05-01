3c6471e977abc7dc5fe4446a3e6dcdbd516d9261
Vegito2367 <tejaaditya.g@gmail.com>
2023-05-01 01:50:54 -0500

    Added header buttons to layout.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,9 @@
 <script>
-  import Link from "./link.svelte";
 </script>
 
 <main>
   Hi its tej, Welcome to my website
-  <div style="display: flex; column-gap:5px;">
-    <Link destination="/about" displayText="About" />
-    <Link destination="/projects" displayText="Projects" />
-    <Link destination="/blogs" displayText="Blogs" />
-  </div>
+ 
   
 </main>
 
